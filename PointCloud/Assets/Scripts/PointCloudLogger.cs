b9b6a3b0be699dabaf349258a9da018a9c4f4c5e
Sebight <59232422+Sebight@users.noreply.github.com>
2022-01-21 17:20:18 +0100

    Idk assets

---
+++ PointCloud/Assets/Scripts/PointCloudLogger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class PointCloudLogger : MonoBehaviour
+{
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
