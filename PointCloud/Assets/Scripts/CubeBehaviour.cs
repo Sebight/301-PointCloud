80328ed4ae062e43433b60ed58d80e9ed5ab7145
Sebight <59232422+Sebight@users.noreply.github.com>
2022-02-07 21:09:00 +0100

    nearestpoint

--- PointCloud/Assets/Scripts/CubeBehaviour.cs
+++ PointCloud/Assets/Scripts/CubeBehaviour.cs
@@ -8,29 +8,63 @@ public class CubeBehaviour : MonoBehaviour
 
     public BoxCollider collider;
 
+    public bool IsPointInCollider(Collider other, Vector3 point)
+    {
+        if (other.ClosestPoint(point) == point)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
     public void CheckCollision(List<PointWrap> points)
     {
+        int pointsAffected = 0;
         for (int i = 0; i < points.Count; ++i)
         {
-            Point p = points[i].acualPoint;
-            Vector3 colliderPos = collider.ClosestPoint(new Vector3(p.x, p.y, p.z));
-            float distance = Vector3.Distance(colliderPos, new Vector3(p.x, p.y, p.z));
-            if (distance < 0.1)
+            #region OldCeck                
+            // Point p = points[i].acualPoint;
+            // Vector3 colliderPos = collider.ClosestPoint(new Vector3(p.x, p.y, p.z));
+            // float distance = Vector3.Distance(colliderPos, new Vector3(p.x, p.y, p.z));
+            // if (distance < 0.1)
+            // {
+            //     points[i].go.GetComponent<MeshRenderer>().material.color = Color.green;
+            //     gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+            // } else
+            // {
+            //     points[i].go.GetComponent<MeshRenderer>().material.color = Color.gray;
+            //     gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+            // }
+            #endregion
+
+            if (IsPointInCollider(collider, new Vector3(points[i].acualPoint.x, points[i].acualPoint.y, points[i].acualPoint.z)))
             {
-                points[i].go.GetComponent<MeshRenderer>().material.color = Color.green;
-                gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
-            } else
+                points[i].go.GetComponent<MeshRenderer>().material.color = Color.blue;
+                pointsAffected++;
+                Debug.Log("yeee");
+            }
+            else
             {
                 points[i].go.GetComponent<MeshRenderer>().material.color = Color.gray;
-                gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
             }
         }
+
+        if (pointsAffected > 0)
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+        else
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+        }
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
