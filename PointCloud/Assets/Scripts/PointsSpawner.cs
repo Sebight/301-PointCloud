ce6162f6e8dc1eafe448fc10f1b810b247417927
Sebight <59232422+Sebight@users.noreply.github.com>
2022-04-16 21:17:37 +0200

    measuring

--- PointCloud/Assets/Scripts/PointsSpawner.cs
+++ PointCloud/Assets/Scripts/PointsSpawner.cs
@@ -46,12 +46,12 @@ public class PointsSpawner : MonoBehaviour
         {
             points[i].position = new Vector3(points[i].x, points[i].y, points[i].z);
         }
-        // points.RemoveRange(10000, points.Count - 10000);
-        for (int i = 0; i < 40_000; i++)
-        {
-            Point point = new Point(new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)), 1f);
-            points.Add(point);
-        }
+        points.RemoveRange(10000, points.Count - 10000);
+        // for (int i = 0; i < 40_000; i++)
+        // {
+        //     Point point = new Point(new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)), 1f);
+        //     points.Add(point);
+        // }
             
         // StartCoroutine(SpawnPoints());
         //for (int i = 0; i < points.Count; i++)
